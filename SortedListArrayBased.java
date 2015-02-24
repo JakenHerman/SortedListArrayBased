f3d2284c10a388420953509aa176571e22f640be
Jaken Herman <jch053@shsu.edu>
2015-02-23 22:50:41 -0600

    in

--- SortedListArrayBased.java
+++ SortedListArrayBased.java
@@ -9,38 +9,40 @@
  */
 
  import java.util.*;
+ import java.util.Collections;
  import java.lang.*;
 
 
 public class SortedListArrayBased extends ListArrayBased
 {
 
-  	public String SortedListArrayBased()
+  	public void SortedListArrayBased()
   	// creates an empty list
   	{
-      ArrayList<String> list = new ArrayList<String>;
-      return list;
-		// TO BE IMPLEMENTED BY YOU
-  	}// end default constructor
+
+      String[] init = null;
+      List list = new ArrayList(Arrays.asList(init));
+
+ 	}// end default constructor
 
   	public void add(Object item) throws ListException
   	// Inserts item into its proper position in a sorted list
   	// Throws an exception if the item connot be placed on the list
   	{
+
+		Arrays.sort(this);
+
 		try
 		{
-      list.add(item);
-
-      //where&when to sort??
-
-			// TO BE IMPLEMENTED BY YOU
+      		this.add(item); //sort where, when?
+            Arrays.sort(this);
 		}
 		catch(Exception e)
 		{
 			throw new ListException("Add to List failed:  " + e.toString());
 		}
 
-	}
+	} //end of add method
 
   	public void remove(Object item) throws ListException
   	// Removes the item from a sorted list.
@@ -48,24 +50,41 @@ public class SortedListArrayBased extends ListArrayBased
   	{
 		try
 		{
-			// TO BE IMPLEMENTED BY YOU
+			this.remove(item);
+
 		}
 		catch(Exception e)
 		{
 			throw new ListException("Remove " + item.toString() + " from List failed:  " + e.toString());
 		}
-	}
+	}//end of remove method
 
     public int locateIndexToAdd(Object item)
 	{
-		// TO BE IMPLEMENTED BY YOU
-	}
+        String i_string = item.toString();
+		Collections.sort(SortedListArrayBased());
+		for (int i = 0; i <= this.size()-1; i++){
+			if (this.get(i).equals(i_string)) {
+				return i;
+			} //end if
+		} // end for
+
+		return 0; //default return type
+
+	} //end of locateIndexToAdd method
 
   	public int locateIndexToRemove(Object item)
   	// Returns the position where the item belongs or exists in a sorted list;
   	// Otherwise, it returns -1.
   	{
-		// TO BE IMPLEMENTED BY YOU
-	}
+		String i_string = item.toString();
+        Collections.sort(SortedListArrayBased());
+        for (int j = 0; i <= this.size()-1; j++){
+            if(this.get(j).equals(i_string)) {
+                return j;
+            } // end if
+        } // end for
+
+	} //end locate Index to remove method.
 
 }  // end SortedListArrayBased
